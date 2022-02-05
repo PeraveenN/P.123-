12e025169d85a8f8934d9229e96b3c6ebac5df76
PeraveenN <87541643+PeraveenN@users.noreply.github.com>
2022-02-05 08:23:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function setup() {
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function draw() {
+    background('aqua');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+
+
